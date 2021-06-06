3a37008d2f8a334a7280ac8eb4a0db28717deec3
vmurback <83590663+vmurback@users.noreply.github.com>
2021-06-06 18:49:46 -0300

    Update

---
+++ Polimorfismo e herança/TestaTudo.java
@@ -0,0 +1,28 @@
+package exercicio1205;
+
+public class TestaTudo {
+
+	public static void main(String[] args) {
+		
+		
+		Cavalo cavalinho = new Cavalo();
+		Cachorro cachorrinho  = new Cachorro();
+		Preguica preguicinha = new Preguica();
+		
+		cavalinho.som();
+		cachorrinho.som();
+		preguicinha.som();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
